6940fc4924d75fc6437056796319cd0473072eb2
Zuzanna Wojtyniak <zuzwoj2@st.amu.edu.pl>
2018-05-15 16:17:43 +0200

    Inny wygląd

---
+++ SZImapa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mapaSZI2" tilewidth="100" tileheight="100" tilecount="48" columns="8">
+ <image source="mapaSZI2.png" width="800" height="600"/>
+</tileset>
