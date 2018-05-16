9a881e0580344a2ad1b30e92dd1c720fcf544f5c
lukaszszy <lookszym@gmail.com>
2018-05-16 14:04:48 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,40 @@
+import heapq
+
+class PriorityQueue:
+    def __init__(self, order='min', f=lambda x: x):
+        self.heap = []
+
+        if order == 'min':
+            self.f = f
+        elif order == 'max':  # now item with max f(x)
+            self.f = lambda x: -f(x)  # will be popped first
+        else:
+            raise ValueError("order must be either 'min' or max'.")
+
+    def append(self, item):
+        heapq.heappush(self.heap, (self.f(item), item))
+
+    def extend(self, items):
+        for item in items:
+            self.heap.append(item)
+
+    def pop(self):
+        if self.heap:
+            return heapq.heappop(self.heap)[1]
+        else:
+            raise Exception('Trying to pop from empty PriorityQueue.')
+
+    def __len__(self):
+        return len(self.heap)
+
+    def __contains__(self, item):
+        return (self.f(item), item) in self.heap
+
+    def __getitem__(self, key):
+        for _, item in self.heap:
+            if item == key:
+                return item
+
+    def __delitem__(self, key):
+        self.heap.remove((self.f(key), key))
+        heapq.heapify(self.heap)
